b90ae39986df595d6a9bf9b83136fbcf707d6e94
David Hagan <instantink+pair+david@hp.com>
2016-02-17 13:28:21 -0800

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+  before_save do
+    email.downcase!
+  end
+
+  VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+
+  has_secure_password
+end
