7516509d77c9e082c19779f666139aabcf0c5267
David Hagan <instantink+pair+david@hp.com>
2016-02-17 14:26:37 -0800

    Updated users to have password requirements.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ActiveRecord::Base
   end
 
   VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 end
